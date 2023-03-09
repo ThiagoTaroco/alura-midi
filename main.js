0f643e3fde8a61b2d2462cf48dc7e664d4af6cb9
TTaroco <ttarocos@gmail.com>
2023-03-09 17:49:23 -0300

    Botões funcionando com seus respectivos sons

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+}
